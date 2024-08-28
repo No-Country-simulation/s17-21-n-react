f6ca49b0c2ff220635423811b9fb08ab53ab49fd
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2024-08-28 15:15:38 -0500

    fix: errorPage and refactor image imports

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -3,9 +3,16 @@ import pinterest from "./icons/pinterest.svg"
 import twitter from "./icons/twitter.svg"
 import btn from "./icons/Button.svg"
 
+import errorImage from "./images/404.webp"
+import man1 from "./images/man1.webp"
+import man2 from "./images/man2.webp"
+
 export {
     facebook,
     pinterest,
     twitter,
-    btn
+    btn,
+    errorImage,
+    man1,
+    man2,
 }
--- frontend/src/components/ErrorPage.jsx
+++ frontend/src/components/ErrorPage.jsx
@@ -1,23 +1,24 @@
-import errorImage from "../assets/images/404.png"
-import errorMan1 from "../assets/images/man1.png"
-import errorMan2 from "../assets/images/man2.png"
+import { errorImage, errorMan1, errorMan2 } from "../assets"
+
 
 const ErrorPage = () => {
     return (
-        <>
-            <div className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-                <div className="text-center">
-                    <img className="mx-auto mb-6 h-48" src={errorImage} alt="Error 404" />
-                    <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Página no encontrada</h1>
-                    <p className="mt-6 text-base leading-7 text-gray-600">Por favor, intenta buscar otra página.</p>
-                    <div className="mt-10 flex items-center justify-center gap-x-6">
-                        <a
-                            href="/"
-                            className="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
-                        >
-                            Volver al Inicio
-                        </a>
-                    </div>
+        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+            <div className="text-center">
+                <img 
+                    className="mx-auto mb-6 h-48" 
+                    src={errorImage} 
+                    alt="Error 404 - Pagina no encontrada"
+                />
+                <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Página no encontrada</h1>
+                <p className="mt-6 text-base leading-7 text-gray-600">Por favor, intenta buscar otra página.</p>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                    <a
+                        href="/"
+                        className="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
+                    >
+                        Volver al Inicio
+                    </a>
                 </div>
             </div>
 
@@ -25,7 +26,7 @@ const ErrorPage = () => {
                 <img
                     className="w-64 h-64 object-contain"
                     src={errorMan1}
-                    alt="Side Image"
+                    alt="Representacion de error"
                 />
             </div>
 
@@ -33,10 +34,10 @@ const ErrorPage = () => {
                 <img
                     className="w-20 h-28  object-contain"
                     src={errorMan2}
-                    alt="Side Image"
+                    alt="Representacion de error"
                 />
             </div>
-        </>
+        </main>
     )
 }
 
