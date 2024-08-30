60f9bca92dc96584e83c4e8f18b11133f28fdf4c
gabyp05 <gabyapd05@gmail.com>
2024-08-30 17:49:10 -0400

    testimonials carrousel

---
+++ frontend/src/components/TestimonialsCarrousel.jsx
@@ -0,0 +1,36 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper/modules";
+import { testimonials } from "../data/testimonials";
+import Testimonials from "./Testimonials";
+
+const TestimonialsCarrousel = () => {
+  return (
+    <div className="container mx-auto my-10">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={20}
+        loop={true}
+        pagination={{
+          dynamicBullets: true,
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: true,
+        }}
+        modules={[Pagination, Autoplay]}
+        className="mySwiper"
+      >
+        {testimonials.map((testimonial) => (
+          <SwiperSlide key={testimonial.id}>
+            <Testimonials testimonial={testimonial} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default TestimonialsCarrousel;
