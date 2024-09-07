3b3cb7c9b4fc6499127dd2edfc6f380c21ed4448
maxcerecedadev <cereceda1991@gmail.com>
2024-09-06 21:13:24 -0500

    style: npm run format

--- frontend/src/components/DashboardSummary.jsx
+++ frontend/src/components/DashboardSummary.jsx
@@ -1,68 +1,73 @@
 import { User, GraduationCap, MessageSquare, CheckSquare } from "lucide-react";
 
 const summary = [
-    {
-        id: 1,
-        title: "ESTUDIANTES INSCRIPTOS",
-        icon: <User className="text-gray-600" size={32} />,
-        value: "1,200",
-        percentage: "+11%",
-        description: "Respecto al período anterior",
-    },
-    {
-        id: 2,
-        title: "PROMEDIO DE CALIFICACIONES",
-        icon: <GraduationCap className="text-gray-600" size={32} />,
-        value: "17.5",
-        percentage: "29%",
-        description: "Respecto al período anterior",
-    },
-    {
-        id: 3,
-        title: "CLASES IMPARTIDAS",
-        icon: <MessageSquare className="text-gray-600" size={32} />,
-        value: "1890",
-        percentage: "0%",
-        description: "Respecto al período anterior",
-    },
-    {
-        id: 4,
-        title: "TAREAS EVALUADAS",
-        icon: <CheckSquare className="text-gray-600" size={32} />,
-        value: "520",
-        percentage: "+89%",
-        description: "Respecto al período anterior",
-    },
+  {
+    id: 1,
+    title: "ESTUDIANTES INSCRITOS",
+    icon: <User className="text-gray-600" size={24} />,
+    value: "1,200",
+    percentage: "+11%",
+    description: "Respecto al período anterior",
+  },
+  {
+    id: 2,
+    title: "PROMEDIO DE CALIFICACIONES",
+    icon: <GraduationCap className="text-gray-600" size={24} />,
+    value: "17.5",
+    percentage: "-29%",
+    description: "Respecto al período anterior",
+  },
+  {
+    id: 3,
+    title: "CLASES IMPARTIDAS",
+    icon: <MessageSquare className="text-gray-600" size={24} />,
+    value: "1890",
+    percentage: "0%",
+    description: "Respecto al período anterior",
+  },
+  {
+    id: 4,
+    title: "TAREAS EVALUADAS",
+    icon: <CheckSquare className="text-gray-600" size={24} />,
+    value: "520",
+    percentage: "+89%",
+    description: "Respecto al período anterior",
+  },
 ];
 
 const getBackgroundColor = (percentage) => {
-    const value = parseFloat(percentage);
-    if (value > 0) return "bg-green-500";
-    if (value < 0) return "bg-red-500";
-    return "bg-yellow-500";
+  const value = parseFloat(percentage);
+  if (value > 0) return "bg-green-500";
+  if (value < 0) return "bg-red-500";
+  return "bg-yellow-500";
 };
 const DashboardSummary = () => {
-    return (
-        <div className="w-full gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 h-full items-center justify-center mx-10">
-            {summary.map((sumary) => (
-                <div className="w-3/4 flex flex-col h-auto justify-center gap-1 shadow-xl" key={sumary.id}>
-                    <div className="w-auto text-center h-auto text-black font-bold text-xl my-10">
-                        <div className=" flex flex-row items-center justify-around">
-                            <h2>{sumary.title}</h2>
-                            <h2 className="text-lg">{sumary.icon}</h2>
-                        </div>
-                        <h2 className="w-10 text-2xl mx-auto text-center">{sumary.value}</h2>
-                    </div>
-                    <div className="flex justify-around mb-6">
-                        <div className={`w-10 h-auto text-sm mx-4 rounded text-center ${getBackgroundColor(sumary.percentage)} text-white`}>
-                            <p className="py-2">{sumary.percentage}</p>
-                        </div>
-                        <p className="text-black text-lg text-center">{sumary.description}</p>
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className="w-full gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 h-full items-center justify-center">
+      {summary.map((sumary) => (
+        <div
+          className="w-full p-4 flex flex-col h-auto gap-1 shadow-lg rounded-lg bg-[#f4f5f9]"
+          key={sumary.id}
+        >
+          <div className="w-auto h-auto text-gray-500 font-bold text-sm">
+            <div className=" flex items-center justify-between">
+              <h2>{sumary.title}</h2>
+              <h2>{sumary.icon}</h2>
+            </div>
+          </div>
+          <h2 className="text-2xl text-gray-500 text-left font-bold">{sumary.value}</h2>
+          <div className="flex justify-start items-center gap-4">
+            <div
+              className={`w-10 h-auto text-sm rounded text-center ${getBackgroundColor(sumary.percentage)} text-white`}
+            >
+              <p className="py-2">{sumary.percentage}</p>
+            </div>
+            <p className="text-black text-sm">{sumary.description}</p>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default DashboardSummary;
--- frontend/src/pages/teachers/DashboardTeachers.jsx
+++ frontend/src/pages/teachers/DashboardTeachers.jsx
@@ -1,5 +1,6 @@
 import ActivityOverview from "../../components/ActivityOverview";
 import AttendanceCardSVG from "../../components/AttendanceCardSVG";
+import DashboardSummary from "../../components/DashboardSummary";
 import EvaluationStatisticsSVG from "../../components/EvaluationStatisticsSVG";
 import LineChartSVG from "../../components/LineChartSVG";
 import { evaluationStatistics, exampleTaskData } from "../../data/dashboardTeachers";
@@ -7,10 +8,7 @@ import { evaluationStatistics, exampleTaskData } from "../../data/dashboardTeach
 const DashboardTeachers = () => {
   return (
     <div className="container bg-white">
-      <header className="text-center mb-8">
-        <h1 className="text-4xl font-bold">Teacher Dashboard</h1>
-      </header>
-
+      <DashboardSummary />
       <section className="bg-white my-7 grid grid-cols-1 lg:grid-cols-3 gap-6">
         <AttendanceCardSVG />
         <EvaluationStatisticsSVG data={evaluationStatistics} />
@@ -23,7 +21,8 @@ const DashboardTeachers = () => {
 
       <footer className="text-center mt-8 border-t-2 pt-4">
         <p className="text-sm">
-          © {new Date().getFullYear()} EducaPro, Todos los derechos reservados. Equipo: s17-n-react-node
+          © {new Date().getFullYear()} EducaPro, Todos los derechos reservados. Equipo:
+          s17-n-react-node
         </p>
       </footer>
     </div>
