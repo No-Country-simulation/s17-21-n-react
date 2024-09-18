72dad0c6df09f44719c75bad01c9b77e62f80f5b
Tomás Zarriello <tomz.ry64@gmail.com>
2024-09-18 12:49:52 -0300

    feat: add student/admin calendar

--- frontend/src/admin/routes/AdminRoutes.jsx
+++ frontend/src/admin/routes/AdminRoutes.jsx
@@ -4,12 +4,16 @@ import NestedRoutes from "../../public/routes/NestedRoutes";
 import TeachersManagement from "../pages/TeachersManagement ";
 import CoursesByAdmin from "../pages/CoursesByAdmin";
 import ActivitiesList from "../../teachers/components/ActivitiesList";
+import CalendarByAdmin from "../pages/CalendarByAdmin";
+
 
 const adminRoutes = [
   { path: "dashboard", element: <DashboardAdmin /> },
   { path: "teachers", element: <TeachersManagement /> },
   { path: "courses", element: <CoursesByAdmin /> },
   { path: "courses/:id/classes", element: <ActivitiesList /> },
+  { path: "calendar", element: <CalendarByAdmin /> },
+
 
   // Otras rutas específicas para administradores
 ];
---
+++ frontend/src/students/pages/CalendarByStudent.jsx
@@ -0,0 +1,11 @@
+import CalendarComponent from "../components/Calendar";
+
+const CalendarByStudent = () => {
+  return (
+    <div>
+      <CalendarComponent />
+    </div>
+  );
+};
+
+export default CalendarByStudent;
