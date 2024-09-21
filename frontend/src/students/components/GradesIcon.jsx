16800057168b31dda7f517d3c076acf6e311c2e8
maxcerecedadev <cereceda1991@gmail.com>
2024-09-21 17:54:25 -0500

    chore: npm run format

--- frontend/src/students/components/GradesIcon.jsx
+++ frontend/src/students/components/GradesIcon.jsx
@@ -2,10 +2,10 @@
 export default function GradesIcon({ item }) {
   const path =
     item.status === "completed"
-      ? "M5 13l4 4L19 7" 
+      ? "M5 13l4 4L19 7"
       : item.status === "failed"
-      ? "M6 18L18 6M6 6l12 12" 
-      : "M12 8v4l3 3M12 6a9 9 0 1 0 9 9" 
+        ? "M6 18L18 6M6 6l12 12"
+        : "M12 8v4l3 3M12 6a9 9 0 1 0 9 9";
 
   return (
     <svg
@@ -16,7 +16,7 @@ export default function GradesIcon({ item }) {
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="w-6 h-6" // Ajusta el tamaño si es necesario
+      className="w-6 h-6"
     >
       <path strokeLinecap="round" strokeLinejoin="round" d={path} />
     </svg>
